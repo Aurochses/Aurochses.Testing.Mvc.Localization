4bdbabe23da1de437ec9bb562bcec0e5f1390263
Vladimir Rodchenko <vladimir@aurochses.com>
2017-10-31 11:57:30 +0300

    rename

---
+++ test/Aurochses.Xunit.AspNetCore.Mvc.Localization.Tests/Fakes/ModelLocalizationAssert/Validate/Models/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aurochses.Xunit.AspNetCore.Mvc.Localization.Tests.Fakes.ModelLocalizationAssert.Validate.Models
+{
+    public class HomeViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email", Prompt = "Email.Prompt", Description = "Email.Description")]
+        public string Email { get; set; }
+
+        public string Token { get; set; }
+    }
+}
